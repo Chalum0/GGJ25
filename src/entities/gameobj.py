09de05c444325e177d1b76641b60900b73374250
Chalum0 <raphaelzanatta24@proton.me>
2025-01-25 15:55:04 +0100

    Fixed parenting of gameObj class and it's children

--- src/entities/gameobj.py
+++ src/entities/gameobj.py
@@ -1,7 +1,11 @@
+import pygame
+
 class GameObj:
 
     def __init__(self):
         self.pos = [0, 0]
+        self.texture = None
+        self.rect = None
 
         self.max_x_momentum = 6
         self.max_y_momentum = 15
@@ -10,9 +14,11 @@ class GameObj:
         self.x_momentum = 0
         self.y_momentum = 0
 
-        self.rect = None
 
-        self.load_texture()
+    def load_texture(self, path):
+        self.texture = pygame.image.load('./src/textures/player.png')
+        self.texture = pygame.transform.scale(self.texture, (20, 20))
+        self.rect = self.texture.get_rect()
 
-    def load_texture(self):
-        self.texture = None
+    def update_rect(self):
+        self.rect.topleft = self.pos
